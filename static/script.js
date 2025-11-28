cf2ac2e377272a128b300577015281b2195da5e1
liuweiqiang0523 <liuweiqiang0523@gmail.com>
2025-11-29 00:46:20 +0800

    Update API base URL in script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 // API配置
-const API_BASE_URL = 'https://22-homepage.liuweiqiang0523.workers.dev';
+const API_BASE_URL = 'https://oknm.de';
 
 // GitHub用户名将从API获取
 let GITHUB_USERNAME = 'IonRh'; // 默认值，将被API数据覆盖
